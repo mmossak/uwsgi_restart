221428f3c988379103d84d82c142c23ec7d69b36
mmossak <mm.mossakowski@gmail.com>
2022-01-01 19:37:22 +0100

    Final version of uwsgi restart script

--- HP_restart_uwsgi.sh
+++ HP_restart_uwsgi.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo "Starting Uwsgi server management...."
+echo 'Starting Uwsgi server management....'
 
-for i in (sudo cat /var/log/uwsgi.log | grep 'uWSGI master process' | grep -Eo "[0-9]{1,}" | tail -2);
+#check last 4 instances of master uwsgi process
+for i in $(cat /var/log/uwsgi.log | grep 'uWSGI master process' | grep -Eo "[0-9]{1,}" | tail -4);
 do
-  echo $i;
+  echo "$i";
+  if ps -p $i > /dev/null 
+    then 
+     echo "found process with pid: $i"
+     kill $i
+     echo "process $i killed"
+     echo "starting uwsgi server again"
+     /usr/local/bin/uwsgi --ini /home/pi/sampleApp/uwsgi_config.ini --uid www-data --gid www-data --daemonize /var/log/uwsgi.log 
+  fi
 done
